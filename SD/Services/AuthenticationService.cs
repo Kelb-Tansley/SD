fc9b49fb919483895a9f647f8e3ba895c462ac4f
Kelb-Tansley <kelbys@hotmail.co.za>
2026-03-30 23:55:00 +0200

    Clean up

--- SD/Services/AuthenticationService.cs
+++ SD/Services/AuthenticationService.cs
@@ -10,7 +10,8 @@ public class AuthenticationService : IAuthenticationService
 {
     private readonly string _appRegClientId = "32631303-9274-4bf0-a8bd-4d1877a7f331";
     private readonly string _appRegTenantId = "2cd924f5-c630-4fca-9460-d4032b489567";
-    private readonly string _apiAppRegClientId = "11c7f14f-3903-4948-8fe0-a106b8b001e2";
+    //private readonly string _apiAppRegClientId = "11c7f14f-3903-4948-8fe0-a106b8b001e2";
+    private readonly string _apiAppRegClientId = "d26d523e-f16a-42ab-a50f-fb869bc2b2f5";
 
     private readonly ITokenCacheService _tokenCacheService;
     private readonly IWebApiHttpClient _webApiHttpClient;
